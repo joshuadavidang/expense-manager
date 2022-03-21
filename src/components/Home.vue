4ce2be53514ebe321e64ad7d31336d64ff3a4423
Joshua David <chunxiong_@outlook.sg>
2022-03-22 00:03:30 +0800

    Dark mode icon updated dynamically

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,21 +5,17 @@
         <p class="pt-16 text-white text-sm">Hello, {{ this.name }} 👋</p>
       </label>
 
-      <label class="flex justify-center">
-        <p class="flex pt-5 text-white text-sm">
-          <router-link to="/addtransaction">
-            <span>
-              <button
-                class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-                v-on:click="addBtn"
-              >
-                Add Transaction
-              </button></span
-            >
-          </router-link>
-          <span> <DarkMode /></span>
-        </p>
-      </label>
+      <div class="flex justify-center flex-row pt-7">
+        <div>
+          <button
+            class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            v-on:click="addBtn"
+          >
+            Add Transaction
+          </button>
+        </div>
+        <div><DarkMode /></div>
+      </div>
 
       <div class="flex justify-center">
         <div
@@ -144,6 +140,7 @@ export default {
     },
     addBtn() {
       // alert("Add clicked");
+      this.$router.push({ name: "AddTransaction" });
     },
 
     getRequestedData() {
